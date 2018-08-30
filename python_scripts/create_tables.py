1345727c9071c666ea3443e71223b0ac47d3d542
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2018-08-30 20:58:08 +0100

    align to latest metadata standard

--- python_scripts/create_tables.py
+++ python_scripts/create_tables.py
@@ -1,4 +1,4 @@
-if __name__ = "__main__":
+if __name__ == "__main__":
     from etl_manager import meta
-    db_meta = meta.DatabaseMeta("glue/meta_data/occupeye_db/", db_suffix = "")
+    db_meta = meta.read_database_folder("glue/meta_data/occupeye_db/")
     db_meta.create_glue_database()
