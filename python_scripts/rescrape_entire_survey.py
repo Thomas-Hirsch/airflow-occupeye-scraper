4e5e3b5db7c6af173c846377f41a7a8e284aab4b
Thomas Hirsch <thomas.hirsch@justice.gov.uk>
2019-03-13 15:51:02 +0000

    add get_survey_from_id method

--- python_scripts/rescrape_entire_survey.py
+++ python_scripts/rescrape_entire_survey.py
@@ -10,6 +10,16 @@ from refresh_partitions import refresh_glue_partitions
 from transfer_to_s3 import surveys_to_s3, sensor_dimension_to_s3, survey_fact_to_s3
 from time_utils import scrape_date_in_surveydays, next_execution_is_in_future, get_survey_dates
 
+def get_survey_from_id(survey_id):
+    surveys = get_surveys_from_api()
+    survey = [s for s in surveys if s['SurveyID'] == survey_id]
+
+    if len(survey) == 1:
+        survey = survey[0]
+    else:
+        raise ValueError("Survey doesn't exist")
+    return survey
+
 def rescrape_entire_survey(survey):
     surveydays = get_survey_dates(survey)
 
